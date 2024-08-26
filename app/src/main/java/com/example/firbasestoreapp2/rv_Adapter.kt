3d8f93f38f0871c70e3946060098fe1f7d45b224
Nishat <shourovhossainnishat@gmail>
2024-08-26 19:24:31 +0600

    delete data

--- app/src/main/java/com/example/firbasestoreapp2/rv_Adapter.kt
+++ app/src/main/java/com/example/firbasestoreapp2/rv_Adapter.kt
@@ -3,8 +3,11 @@ package com.example.firbasestoreapp2
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.firbasestoreapp2.databinding.RvItemBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class RvAdapter(var context: Context, var list: ArrayList<users>) :
     RecyclerView.Adapter<RvAdapter.ViewHolder>() {
@@ -24,6 +27,18 @@ class RvAdapter(var context: Context, var list: ArrayList<users>) :
         val user = list[position]
         holder.binding.name.text = user.name  ?: "No Name"
         holder.binding.pass.text = user.pass  ?: "No Password"
+        holder.binding.delete.setOnClickListener {
+            val db = Firebase.firestore
+            db.collection("users").document(list.get(position).id!!).delete()
+
+                .addOnSuccessListener {
+                    Toast.makeText(context, "Deleted ", Toast.LENGTH_SHORT).show()
+                    notifyItemRemoved(position)
+                } .addOnFailureListener {
+                    Toast.makeText(context, "Failed to delete ", Toast.LENGTH_SHORT).show()
+                }
+
+        }
     }
 }
 
